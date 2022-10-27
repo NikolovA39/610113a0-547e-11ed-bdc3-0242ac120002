8f63c161c34d9279be9fd2058670f0af0b126ebb
Boris Nikolov <nikolov_b@abv.bg>
2022-10-27 10:38:03 +0300

    Initial commit

---
+++ src/main/java/com/example/proecttask/ProEctTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.proecttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProEctTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProEctTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proecttask/ProEctTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proecttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProEctTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
