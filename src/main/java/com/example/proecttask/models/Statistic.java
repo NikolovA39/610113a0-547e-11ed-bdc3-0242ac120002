6cf1844800db6c1c858f8b27f618c2a0e35eacbc
nikolov_b@abv.bg <nikolov_b@abv.bg>
2022-10-28 10:45:31 +0300

    add fields in Statistic model

--- src/main/java/com/example/proecttask/models/Statistic.java
+++ src/main/java/com/example/proecttask/models/Statistic.java
@@ -1,7 +1,6 @@
 package com.example.proecttask.models;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 @Table(name = "statistics")
@@ -17,20 +16,21 @@ public class Statistic {
 
     @Column(name = "country_code")
     private String countryCode;
-
+    @Column(name = "new_confirmed")
     private int newConfirmed;
-
+    @Column(name = "total_confirmed")
     private int totalConfirmed;
-
+    @Column(name = "new_deaths")
     private int newDeaths;
-
+    @Column(name = "total_deaths")
     private int totalDeaths;
-
+    @Column(name = "new_recovered")
     private int newRecovered;
-
+    @Column(name = "total_recovered")
     private int totalRecovered;
 
-    private Date date;
+    @Column(name = "date")
+    private String date;
 
     public Statistic() {
     }
@@ -57,5 +57,61 @@ public class Statistic {
 
     public void setCountryCode(String countryCode) {
         this.countryCode = countryCode;
+    }
+
+    public int getNewConfirmed() {
+        return newConfirmed;
+    }
+
+    public void setNewConfirmed(int newConfirmed) {
+        this.newConfirmed = newConfirmed;
+    }
+
+    public int getTotalConfirmed() {
+        return totalConfirmed;
+    }
+
+    public void setTotalConfirmed(int totalConfirmed) {
+        this.totalConfirmed = totalConfirmed;
+    }
+
+    public int getNewDeaths() {
+        return newDeaths;
+    }
+
+    public void setNewDeaths(int newDeaths) {
+        this.newDeaths = newDeaths;
+    }
+
+    public int getTotalDeaths() {
+        return totalDeaths;
+    }
+
+    public void setTotalDeaths(int totalDeaths) {
+        this.totalDeaths = totalDeaths;
+    }
+
+    public int getNewRecovered() {
+        return newRecovered;
+    }
+
+    public void setNewRecovered(int newRecovered) {
+        this.newRecovered = newRecovered;
+    }
+
+    public int getTotalRecovered() {
+        return totalRecovered;
+    }
+
+    public void setTotalRecovered(int totalRecovered) {
+        this.totalRecovered = totalRecovered;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
